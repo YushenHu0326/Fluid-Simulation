d8573ad5886d20617e860afa126709c66c7696cf
YushenHu0326 <62900433+YushenHu0326@users.noreply.github.com>
2024-04-21 04:36:23 -0400

    Update fluid2d.m

--- fluid2d.m
+++ fluid2d.m
@@ -1,14 +1,44 @@
 function fluid2d()
 
-GRAVITY=0.1;
-DT=1;
+GRAVITY=0.01;
+DT=0.1;
 TIME_STEP_TOTAL=1000;
-PARTICLE_NUM=50000;
+PARTICLE_NUM=10000;
 GRID_H=100;
 GRID_W=100;
 
 function outbound = is_out_of_bound(pos)
-    outbound=(pos(1)<0||pos(1)>GRID_W)||(pos(2)<0||pos(2)>GRID_H);
+    outbound=(pos(1)<0||pos(1)>GRID_W-1)||(pos(2)<0||pos(2)>GRID_H-1);
+end
+
+function pos = force_push_inbound_pos(pos)
+    if pos(1)<0
+        pos(1)=0.001;
+    end
+    if pos(1)>GRID_W-1
+        pos(1)=GRID_W-1.001;
+    end
+    if pos(2)<0
+        pos(2)=0.001;
+    end
+    if pos(2)>GRID_H-1
+        pos(2)=GRID_H-1.001;
+    end
+end
+
+function vel = force_push_inbound_vel(pos,vel)
+    if pos(1)<0
+        vel(1)=-vel(1)*0.5;
+    end
+    if pos(1)>GRID_W-1
+        vel(1)=-vel(1)*0.5;
+    end
+    if pos(2)<0
+        vel(2)=-vel(2)*0.5;
+    end
+    if pos(2)>GRID_H-1
+        vel(2)=-vel(2)*0.5;
+    end
 end
 
 %function p_v = interpolate_v(p_pos)
@@ -33,16 +63,12 @@ particle_v=zeros(PARTICLE_NUM,2);
 for i=1:TIME_STEP_TOTAL
     % update particle velocity
     for j=1:PARTICLE_NUM
-        % if particle is out of bound, push the particle back
-        if is_out_of_bound(particles(j,:))
-            particle_v(j,2)=-particle_v(j,2)*0.5;
-        else
-            % add gravity
-            particle_v(j,2)=particle_v(j,2)+GRAVITY*DT;
-            % add grid velocity
-            particle_v(j,1)=particle_v(j,1)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),1)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),2);
-            particle_v(j,2)=particle_v(j,2)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),3)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),4);
-        end
+        %disp(particle_v(j,2))
+        % add gravity
+        particle_v(j,2)=particle_v(j,2)+GRAVITY*DT;
+        % add grid velocity
+        particle_v(j,1)=particle_v(j,1)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),1)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),2);
+        particle_v(j,2)=particle_v(j,2)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),3)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),4);
     end
     % update position
     particles=particles+particle_v*DT;
@@ -52,13 +78,16 @@ for i=1:TIME_STEP_TOTAL
     grid_v_copy=zeros(GRID_W,GRID_H,4);
     % update particles in grid
     for j=1:PARTICLE_NUM
-        if ~is_out_of_bound(particles(j,:))
-            % push particles to grid
-            grid(ceil(particles(j,2)),ceil(particles(j,1)))=grid(ceil(particles(j,2)),ceil(particles(j,1)))+1;
-            % push particle velocity to corresponding grid velocity
-            grid_v(ceil(particles(j,2)),ceil(particles(j,1)),1)=grid_v(ceil(particles(j,2)),ceil(particles(j,1)),1)+particle_v(j,1)/1000;
-            grid_v(ceil(particles(j,2)),ceil(particles(j,1)),2)=grid_v(ceil(particles(j,2)),ceil(particles(j,1)),2)+particle_v(j,2)/1000;
+        if is_out_of_bound(particles(j,:))
+            % if particle is out of bound, push the particle back
+            particle_v(j,:)=force_push_inbound_vel(particles(j,:),particle_v(j,:));
+            particles(j,:)=force_push_inbound_pos(particles(j,:));
         end
+        % push particles to grid
+        grid(ceil(particles(j,2)),ceil(particles(j,1)))=grid(ceil(particles(j,2)),ceil(particles(j,1)))+1;
+        % push particle velocity to corresponding grid velocity
+        grid_v(ceil(particles(j,2)),ceil(particles(j,1)),1)=grid_v(ceil(particles(j,2)),ceil(particles(j,1)),1)+particle_v(j,1);
+        grid_v(ceil(particles(j,2)),ceil(particles(j,1)),2)=grid_v(ceil(particles(j,2)),ceil(particles(j,1)),2)+particle_v(j,2);
     end
     % make grid incompressible
     for j=1:GRID_H
@@ -86,6 +115,7 @@ for i=1:TIME_STEP_TOTAL
                 cnt=cnt+1;
             end
             d=d/cnt;
+            disp(d)
             if j~=1
                 grid_v_copy(j-1,k,3)=grid_v_copy(j-1,k,3)+d;
             end
@@ -103,7 +133,7 @@ for i=1:TIME_STEP_TOTAL
     
     imshow(grid/20);
     %h.GridVisible='off';
-    disp(max(grid_v,[],'all'))
+    %disp(max(grid_v_copy,[],'all'))
     drawnow
 
 end
