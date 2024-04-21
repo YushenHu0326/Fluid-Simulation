17eacee4a0c33b7b166efe84e403383347a22e74
YushenHu0326 <62900433+YushenHu0326@users.noreply.github.com>
2024-04-21 04:00:15 -0400

    Add files via upload

---
+++ fluid2d.m
@@ -0,0 +1,111 @@
+function fluid2d()
+
+GRAVITY=0.1;
+DT=1;
+TIME_STEP_TOTAL=1000;
+PARTICLE_NUM=50000;
+GRID_H=100;
+GRID_W=100;
+
+function outbound = is_out_of_bound(pos)
+    outbound=(pos(1)<0||pos(1)>GRID_W)||(pos(2)<0||pos(2)>GRID_H);
+end
+
+%function p_v = interpolate_v(p_pos)
+%    p_x=ceil(p_pos(1));
+%    p_y=ceil(p_pos(2));
+%end
+
+clear global;
+close all;
+
+grid=zeros(GRID_W,GRID_H);
+grid_v=zeros(GRID_W,GRID_H,2);
+grid_v_copy=zeros(GRID_W,GRID_H,4);
+
+particles=zeros(PARTICLE_NUM,2);
+for i=1:PARTICLE_NUM
+    particles(i,1)=rand*GRID_W;
+    particles(i,2)=rand*GRID_H;
+end
+particle_v=zeros(PARTICLE_NUM,2);
+
+for i=1:TIME_STEP_TOTAL
+    % update particle velocity
+    for j=1:PARTICLE_NUM
+        % if particle is out of bound, push the particle back
+        if is_out_of_bound(particles(j,:))
+            particle_v(j,2)=-particle_v(j,2)*0.5;
+        else
+            % add gravity
+            particle_v(j,2)=particle_v(j,2)+GRAVITY*DT;
+            % add grid velocity
+            particle_v(j,1)=particle_v(j,1)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),1)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),2);
+            particle_v(j,2)=particle_v(j,2)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),3)+grid_v_copy(ceil(particles(j,2)),ceil(particles(j,1)),4);
+        end
+    end
+    % update position
+    particles=particles+particle_v*DT;
+    % clear grid
+    grid=zeros(GRID_W,GRID_H);
+    grid_v=zeros(GRID_W,GRID_H,2);
+    grid_v_copy=zeros(GRID_W,GRID_H,4);
+    % update particles in grid
+    for j=1:PARTICLE_NUM
+        if ~is_out_of_bound(particles(j,:))
+            % push particles to grid
+            grid(ceil(particles(j,2)),ceil(particles(j,1)))=grid(ceil(particles(j,2)),ceil(particles(j,1)))+1;
+            % push particle velocity to corresponding grid velocity
+            grid_v(ceil(particles(j,2)),ceil(particles(j,1)),1)=grid_v(ceil(particles(j,2)),ceil(particles(j,1)),1)+particle_v(j,1)/1000;
+            grid_v(ceil(particles(j,2)),ceil(particles(j,1)),2)=grid_v(ceil(particles(j,2)),ceil(particles(j,1)),2)+particle_v(j,2)/1000;
+        end
+    end
+    % make grid incompressible
+    for j=1:GRID_H
+        for k=1:GRID_W
+            d=0;
+            cnt=0;
+            if j~=1
+                grid_v_copy(j-1,k,3)=grid_v(j-1,k,2);
+                d=d-grid_v(j-1,k,2);
+                cnt=cnt+1;
+            end
+            if j~=GRID_H
+                grid_v_copy(j+1,k,4)=grid_v(j+1,k,2);
+                d=d+grid_v(j+1,k,2);
+                cnt=cnt+1;
+            end
+            if k~=1
+                grid_v_copy(j,k-1,1)=grid_v(j,k-1,1);
+                d=d-grid_v(j,k-1,1);
+                cnt=cnt+1;
+            end
+            if k~=GRID_W
+                grid_v_copy(j,k+1,2)=grid_v(j,k+1,1);
+                d=d+grid_v(j,k+1,1);
+                cnt=cnt+1;
+            end
+            d=d/cnt;
+            if j~=1
+                grid_v_copy(j-1,k,3)=grid_v_copy(j-1,k,3)+d;
+            end
+            if j~=GRID_H
+                grid_v_copy(j+1,k,4)=grid_v_copy(j+1,k,4)-d;
+            end
+            if k~=1
+                grid_v_copy(j,k-1,1)=grid_v_copy(j,k-1,1)+d;
+            end
+            if k~=GRID_W
+                grid_v_copy(j,k+1,2)=grid_v_copy(j,k+1,2)-d;
+            end
+        end
+    end
+    
+    imshow(grid/20);
+    %h.GridVisible='off';
+    disp(max(grid_v,[],'all'))
+    drawnow
+
+end
+
+end
